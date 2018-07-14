e0c5f246474544314b95be2a417030141493fd25
Unknown <victorgesuato3@gmail.com>
2018-07-14 14:51:13 -0400

    AcabandoAnimacao

--- app/src/main/java/com/cryptog/exercise5machinemoney/MainActivity.java
+++ app/src/main/java/com/cryptog/exercise5machinemoney/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cryptog.exercise5machinemoney;
 
 import android.graphics.drawable.Drawable;
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -104,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
                         textViewResult.setText(Integer.toString(score));
                     }
                 }
+                rollImage();
 
             }
         });
@@ -119,5 +122,275 @@ public class MainActivity extends AppCompatActivity {
         return imagesResult[numeber];
     }
 
+    public void rollImage(){
+
+        final int[] rollImageNumber = {0};
+
+        final ImageView imageView0 = findViewById(R.id.imageView0);
+        final ImageView imageView1 = findViewById(R.id.imageView1);
+        final ImageView imageView2 = findViewById(R.id.imageView2);
+        final ImageView imageView3 = findViewById(R.id.imageView3);
+        final ImageView imageView4 = findViewById(R.id.imageView4);
+        final ImageView imageView5 = findViewById(R.id.imageView5);
+        final ImageView imageView6 = findViewById(R.id.imageView6);
+        final ImageView imageView7 = findViewById(R.id.imageView7);
+        final ImageView imageView8 = findViewById(R.id.imageView8);
+
+
+        // 1 table row
+
+        //image0
+        final SpringAnimation anim1Image0 = new SpringAnimation(imageView0, DynamicAnimation.TRANSLATION_X,  600);
+        final SpringAnimation anim2Image0 = new SpringAnimation(imageView0, DynamicAnimation.TRANSLATION_X,  0);
+
+
+        //image1
+        final SpringAnimation anim1Image1 = new SpringAnimation(imageView1, DynamicAnimation.TRANSLATION_X,  -300);
+        final SpringAnimation anim2Image1 = new SpringAnimation(imageView1, DynamicAnimation.TRANSLATION_X,  300);
+        final SpringAnimation anim3Image1 = new SpringAnimation(imageView1, DynamicAnimation.TRANSLATION_X,  0);
+
+        //image2
+        final SpringAnimation anim1Image2 = new SpringAnimation(imageView2, DynamicAnimation.TRANSLATION_X,  -600);
+        final SpringAnimation anim2Image2 = new SpringAnimation(imageView2, DynamicAnimation.TRANSLATION_X,  0);
+
+
+
+        // 2 table Row
+
+        //image3
+        final SpringAnimation anim1Image3 = new SpringAnimation(imageView3, DynamicAnimation.TRANSLATION_X,  600);
+        final SpringAnimation anim2Image3 = new SpringAnimation(imageView3, DynamicAnimation.TRANSLATION_X,  0);
+
+        //image4
+        final SpringAnimation anim1Image4 = new SpringAnimation(imageView4, DynamicAnimation.TRANSLATION_X,  -300);
+        final SpringAnimation anim2Image4 = new SpringAnimation(imageView4, DynamicAnimation.TRANSLATION_X,  300);
+        final SpringAnimation anim3Image4 = new SpringAnimation(imageView4, DynamicAnimation.TRANSLATION_X,  0);
+
+        // image5
+        final SpringAnimation anim1Image5 = new SpringAnimation(imageView5, DynamicAnimation.TRANSLATION_X,  -600);
+        final SpringAnimation anim2Image5 = new SpringAnimation(imageView5, DynamicAnimation.TRANSLATION_X,  0);
+
+
+        // table row 3
+        //image6
+        final SpringAnimation anim1Image6 = new SpringAnimation(imageView6, DynamicAnimation.TRANSLATION_X,  600);
+        final SpringAnimation anim2Image6 = new SpringAnimation(imageView6, DynamicAnimation.TRANSLATION_X,  0);
+
+        //image7
+        final SpringAnimation anim1Image7 = new SpringAnimation(imageView7, DynamicAnimation.TRANSLATION_X,  -300);
+        final SpringAnimation anim2Image7 = new SpringAnimation(imageView7, DynamicAnimation.TRANSLATION_X,  300);
+        final SpringAnimation anim3Image7 = new SpringAnimation(imageView7, DynamicAnimation.TRANSLATION_X,  0);
+
+        // image8
+        final SpringAnimation anim1Image8 = new SpringAnimation(imageView8, DynamicAnimation.TRANSLATION_X,  -600);
+        final SpringAnimation anim2Image8 = new SpringAnimation(imageView8, DynamicAnimation.TRANSLATION_X,  0);
+
+
+        // animacoes 1 start
+        anim1Image0.start();
+        anim1Image1.start();
+        anim1Image2.start();
+        anim1Image3.start();
+        anim1Image4.start();
+        anim1Image5.start();
+        anim1Image6.start();
+        anim1Image7.start();
+        anim1Image8.start();
+
+
+        // table row 1
+        //image0
+        anim1Image0.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image0.start();
+
+            }
+        });
+
+        anim2Image0.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+               if(rollImageNumber[0] < 3) {
+                   anim1Image0.start();
+               }
+            }
+        });
+
+        // image1
+
+        anim1Image1.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image1.start();
+            }
+        });
+
+        anim2Image1.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim3Image1.start();
+            }
+        });
+        anim3Image1.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+         if(rollImageNumber[0] < 3){
+             anim1Image1.start();
+         }
+         rollImageNumber[0]++;
+
+            }
+        });
+
+        // image2
+
+        anim1Image2.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image2.start();
+
+            }
+        });
+
+        anim2Image2.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+            if(rollImageNumber[0] < 3) {
+                anim1Image2.start();
+            }
+            }
+        });
+
+
+        // table row 2
+        // image3
+
+        anim1Image3.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image3.start();
+
+            }
+        });
+
+        anim2Image3.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                if(rollImageNumber[0] < 3) {
+                    anim1Image3.start();
+                }
+            }
+        });
+
+        // image4
+
+        anim1Image4.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image4.start();
+            }
+        });
+
+        anim2Image4.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim3Image4.start();
+            }
+        });
+        anim3Image4.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                if(rollImageNumber[0] < 3){
+                    anim1Image4.start();
+                }
+
+            }
+        });
+
+        // image5
+
+        anim1Image5.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image5.start();
+
+            }
+        });
+
+        anim2Image5.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                if(rollImageNumber[0] < 3) {
+                    anim1Image5.start();
+                }
+            }
+        });
+
+
+        // table row 3
+        // image6
+
+        anim1Image6.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image6.start();
+
+            }
+        });
+
+        anim2Image6.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                if(rollImageNumber[0] < 3) {
+                    anim1Image6.start();
+                }
+            }
+        });
+
+        // image7
+
+        anim1Image7.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image7.start();
+            }
+        });
+
+        anim2Image7.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim3Image7.start();
+            }
+        });
+        anim3Image7.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                if(rollImageNumber[0] < 3){
+                    anim1Image7.start();
+                }
+
+            }
+        });
+
+        // image8
+
+        anim1Image8.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                anim2Image8.start();
+
+            }
+        });
+
+        anim2Image8.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                if(rollImageNumber[0] < 3) {
+                    anim1Image8.start();
+                }
+            }
+        });
+    }
 
 }
